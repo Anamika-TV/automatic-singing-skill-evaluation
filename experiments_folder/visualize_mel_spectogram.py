4e180efa4f59cef4b198034c5e4526cf38c0d9e5
Anamika-TV <anamikanuz24@gmail.com>
2026-02-23 21:58:00 +0530

    Implement initial regression model for ASSE - Trained model using regression on labels.csv. - Observed performance issues (constant score prediction). - Next step: Pivot to a classification-based approach to improve evaluation accuracy.

--- experiments_folder/visualize_mel_spectogram.py
+++ experiments_folder/visualize_mel_spectogram.py
@@ -4,6 +4,8 @@ import librosa.display
 
 mel = np.load("features/mel_spectrograms/_bmNEUrlKCA.npy")
 
+print(mel.shape)
+
 plt.figure(figsize=(10, 4))
 librosa.display.specshow(mel, sr=22050, hop_length=512,
                          x_axis='time', y_axis='mel')
@@ -12,3 +14,5 @@ plt.title("Log-Mel Spectrogram")
 plt.tight_layout()
 plt.show()
 #plt.savefig("mel_example.png")
+
+
